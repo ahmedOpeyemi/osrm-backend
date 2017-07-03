32695b68d822e78b1546bdf2188bd4ab234e8dff
Kajari Ghosh <ghoshkaj@gmail.com>
2017-07-03 15:03:03 -0400

    Add additional handlers

--- src/extractor/edge_based_graph_factory.cpp
+++ src/extractor/edge_based_graph_factory.cpp
@@ -434,6 +434,7 @@ void EdgeBasedGraphFactory::GenerateEdgeExpandedEdges(
 
     guidance::TurnAnalysis turn_analysis(*m_node_based_graph,
                                          m_coordinates,
+                                         m_osm_node_ids,
                                          node_restriction_map,
                                          m_barrier_nodes,
                                          m_compressed_edge_container,
@@ -443,8 +444,12 @@ void EdgeBasedGraphFactory::GenerateEdgeExpandedEdges(
                                          validate_intersections);
 
     util::guidance::LaneDataIdMap lane_data_map;
-    guidance::lanes::TurnLaneHandler turn_lane_handler(
-        *m_node_based_graph, lane_description_map, turn_analysis, lane_data_map);
+    guidance::lanes::TurnLaneHandler turn_lane_handler(*m_node_based_graph,
+                                                       m_coordinates,
+                                                       m_osm_node_ids,
+                                                       lane_description_map,
+                                                       turn_analysis,
+                                                       lane_data_map);
 
     bearing_class_by_node_based_node.resize(m_node_based_graph->GetNumberOfNodes(),
                                             std::numeric_limits<std::uint32_t>::max());
